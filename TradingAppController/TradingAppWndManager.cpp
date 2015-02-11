79ea2b660c7f2dfc2b7b5638f3fd496cc873709b
hoyong-lee <snipe1205@hotmail.com>
2015-02-11 10:26:41 +0900

    first

---
+++ TradingAppController/TradingAppWndManager.cpp
@@ -0,0 +1,85 @@
+﻿#include "stdafx.h"
+#include "TradingAppWndManager.h"
+
+TradingAppWndManager* TradingAppWndManager::s_pInstance = NULL;
+
+TradingAppWndManager::TradingAppWndManager()
+{
+
+}
+
+
+TradingAppWndManager::~TradingAppWndManager()
+{
+
+}
+
+TradingAppWndManager* TradingAppWndManager::getInstance()
+{
+	if (s_pInstance == NULL)
+		s_pInstance = new TradingAppWndManager();
+
+	return s_pInstance;
+}
+
+bool EnumWindowsProc(HWND hWnd, LPDWORD lParam)
+{
+	TradingAppWndManager::getInstance()->EnumWindowProc(hWnd);
+	return true;
+}
+
+void TradingAppWndManager::EnumWindowProc(HWND hWnd)
+{
+	m_listTradinAppWnds.push_back(hWnd);
+}
+
+void TradingAppWndManager::getTradeAppTopWndHandle()
+{
+	m_listTradinAppWnds.clear();
+	::EnumWindows((WNDENUMPROC)EnumWindowsProc, NULL);
+
+	vector<HWND> topWnds;
+	for (int i = 0; i < m_listTradinAppWnds.size(); i++)
+	{	
+		TCHAR szWindowText[MAX_PATH];
+		::GetWindowText(m_listTradinAppWnds[i], szWindowText, MAX_PATH);
+
+		if (_tcsstr(szWindowText, _T("网上股票交易系统5.0")) != NULL)
+			topWnds.push_back(m_listTradinAppWnds[i]);
+	}
+
+	if (topWnds.size() == 1)
+		m_hWndTopWindow = topWnds[0];
+
+}
+
+bool TradingAppWndManager::getMDIFrameHwnd()
+{
+	m_hWndMDIFrame = ::FindWindowEx(m_hWndTopWindow, NULL, _T("AfxMDIFrame42s"), NULL);
+	return (m_hWndMDIFrame != NULL);
+}
+
+bool TradingAppWndManager::getStatusbarWndHandle()
+{
+	m_hWndStatusbar = ::FindWindowEx(m_hWndTopWindow, NULL, _T("msctls_statusbar32"), NULL);
+	return m_hWndStatusbar != NULL;
+}
+
+bool TradingAppWndManager::getTreeViewWndHandle()
+{
+	HWND hWnd1 = ::FindWindowEx(m_hWndMDIFrame, NULL, _T("AfxWnd42s"), NULL);
+	if (hWnd1 == NULL)
+		return false;
+
+	HWND hWnd2 = ::FindWindowEx(hWnd1, NULL, _T("Afx:400000:0"), NULL);
+	if (hWnd2 == NULL)
+		return false;
+
+	HWND hWnd3 = ::FindWindowEx(hWnd2, NULL, _T("AfxWnd42s"), NULL);
+	if (hWnd3 == NULL)
+		return false;
+
+	m_hWndTreeview = ::FindWindowEx(hWnd3, NULL, _T("SysTreeView32"), NULL);
+	return m_hWndTreeview != NULL;
+}
+
---
+++ TradingAppController/TradingAppWndManager.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+enum LINKRESULT
+{
+	SUCCEEDED = 0,
+	NOT_FOUND_WINDOW,
+	FOUND_MULTIPLE_WINDOWS,
+};
+
+class TradingAppWndManager
+{
+public:
+	TradingAppWndManager();
+	~TradingAppWndManager();
+
+	static TradingAppWndManager* getInstance();
+	void EnumWindowProc(HWND hWnd);
+
+protected:
+
+	void getTradeAppTopWndHandle();
+	bool getMDIFrameHwnd();
+	bool getStatusbarWndHandle();
+	bool getTreeViewWndHandle();
+
+protected:
+
+	static TradingAppWndManager* s_pInstance;
+
+protected:
+	
+	vector<HWND> m_listTradinAppWnds;
+
+	HWND m_hWndTopWindow;
+	HWND m_hWndMDIFrame;
+	HWND m_hWndStatusbar;
+	HWND m_hWndTreeview;
+	HWND m_hWndBuyDlg;
+	HWND m_hWndSellDlg;
+	HWND m_hWndWithdrawDlg;
+	HWND m_hWndMoneyBalanceDlg;
+	HWND m_hWnd_Buy_Stock_Code;
+	HWND m_hWnd_Buy_Stock_Name;
+	HWND m_hWnd_Buy_Stock_Price;
+	HWND m_hWnd_Buy_Available_Stock;
+	HWND m_hWnd_Buy_Stock_Amount;
+	HWND m_hWnd_Buy_Buy_Button;
+	HWND m_hWnd_Sell_Stock_Code;
+	HWND m_hWnd_Sell_Stock_Name;
+	HWND m_hWnd_Sell_Stock_Price;
+	HWND m_hWnd_Sell_Available_Stock;
+	HWND m_hWnd_Sell_Stock_Amount;
+	HWND m_hWnd_Sell_Sell_Button;
+	HWND m_hWnd_Withdraw_SelectAll_Button;
+	HWND m_hWnd_Withdraw_Withdraw_Button;
+	HWND m_hWndMoneyBalance_Balance;
+	HWND m_hWnd_MoneyBalance_Frozen;
+	HWND m_hWndMoneyBalance_Available;
+	HWND m_hWnd_MoneyBalance_Desired;
+	HWND m_hWnd_MoneyBalance_StockCost;
+	HWND m_hWnd_MoneyBalance_MarketCost;
+
+};
+
